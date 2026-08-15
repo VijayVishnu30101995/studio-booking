ebca732709a656770b969cc4ae2bf8c0b19f527c
Vishnu V <vishnuv@Vishnus-Mac-mini.local>
2026-08-15 19:53:08 +0530

    Add IANA timezone validation

---
+++ apps/studios/validators.py
@@ -0,0 +1,12 @@
+from zoneinfo import ZoneInfo, ZoneInfoNotFoundError
+
+from django.core.exceptions import ValidationError
+
+
+def validate_iana_timezone(value: str) -> None:
+    try:
+        ZoneInfo(value)
+    except ZoneInfoNotFoundError:
+        raise ValidationError(
+            "Enter a valid IANA timezone."
+        )
